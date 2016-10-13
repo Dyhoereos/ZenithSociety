9d111b98d136fbc68cae002dc7d6cb5eb62fe6b2
Dyhoereos <dennis.chau@outlook.com>
2016-10-13 10:07:39 -0700

    Creation of admin + member roles

--- ZenithSociety/Startup.cs
+++ ZenithSociety/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using ZenithDataLib.Models;
 
 [assembly: OwinStartupAttribute(typeof(ZenithSociety.Startup))]
 namespace ZenithSociety
@@ -9,6 +12,67 @@ namespace ZenithSociety
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        // Create default User roles and Admin user and Member user
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // Create default Admin Role and create a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "a";
+                user.Email = "a@a.a";
+
+                string userPWD = "P@$$w0rd";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            // Create User role    
+            if (!roleManager.RoleExists("Member"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Member";
+                roleManager.Create(role);
+
+                //Create a member user 
+
+                var user = new ApplicationUser();
+                user.UserName = "m";
+                user.Email = "m@m.m";
+
+                string userPWD = "P@$$w0rd";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Member   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Member");
+                }
+            }
         }
     }
 }
