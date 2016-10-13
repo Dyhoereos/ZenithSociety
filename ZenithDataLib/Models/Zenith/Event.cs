41b61a735224af848863ef85de1458f16a058e48
Dyhoereos <dennis.chau@outlook.com>
2016-10-13 15:26:18 -0700

    moved model from web to datalib; seeded db

---
+++ ZenithDataLib/Models/Zenith/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZenithSociety.Models.Zenith
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime EventFrom { get; set; }
+        public DateTime EventTo { get; set; }
+        public String Creator { get; set; }
+        public Activity Activity { get; set; }
+        public String ActivityDesc { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Boolean IsActive { get; set; }
+
+
+    }
+}
---
+++ ZenithSociety/Migrations/Zenith/201610132220032_seeding.cs
@@ -0,0 +1,16 @@
+namespace ZenithSociety.Migrations.Zenith
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seeding : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
