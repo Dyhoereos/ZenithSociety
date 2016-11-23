d699ff7a7f4d5e106686b4bad938c35f1c77ebd8
Dyhoereos <dennis.chau@outlook.com>
2016-11-23 13:25:31 -0800

    Implemented Roles controller Index/view

---
+++ ASP Core/ZenithSociety/src/ZenithWebsite/Controllers/RolesController.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZenithWebsite.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ZenithWebsite.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<RolesController> _logger;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, ILogger<RolesController> logger)
+        {
+            _roleManager = roleManager;
+            _logger  = logger;
+        }
+
+        // GET: Roles
+        public IActionResult Index()
+        {
+            var roleList = new Dictionary<String, String>();
+
+            foreach (var role in _roleManager.Roles.ToList())
+            {
+                roleList.Add(role.Name.ToString(), role.Id.ToString());
+            }
+
+            ViewData["RolesList"] = roleList;
+            return View();
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
