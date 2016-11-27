ec4f21d9530c864bc5eef5135ff852b6cd514783
Dyhoereos <dennis.chau@outlook.com>
2016-11-27 01:25:22 -0800

    Display current week's events

--- ASP Core/ZenithSociety/ZenithClientSite/src/app/event/event.component.ts
+++ ASP Core/ZenithSociety/ZenithClientSite/src/app/event/event.component.ts
@@ -3,82 +3,43 @@ import {Event} from '../event';
 import {EventService} from '../event.service';
 import {WeekComponent} from './week/week.component'
 import {AuthenticationService} from '../authentication.service'
-import {Activity} from '../activity';
-import {ActivityService} from '../activity.service';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css'],
-  
+    
 })
 export class EventComponent implements OnInit {
-  results: Array<Event>;
-  weekResults: Array<Event>;
-  activityResult: Array<Activity>;
-  // loggedIn = false;
+  loggedIn = false;
+  currentDate: Date = new Date();
 
   @ViewChildren(WeekComponent)
     viewChildren: QueryList<WeekComponent>;
 
-  constructor(private eventService: EventService, private user: AuthenticationService, private activityService: ActivityService) {}
+  constructor(private eventService: EventService, private user: AuthenticationService) {}
 
   ngOnInit() {
-    // this.loggedIn = this.user.isLoggedIn();
-
-  	this.getAllEvents();
-    this.getAllActivities();
-    this.assignActivityToEvent();
-  }
- 
-
-  getAllEvents(): void {
-    // this.eventService.getAll().subscribe(
-    //       data => { this.results = data; },
-    //       error => console.log(error)
-    //       );
-
-    this.eventService.getAll()
-      .then(data => this.results = data);
-  }
-
-  getAllActivities(): void {
-    this.activityService.getAll()
-      .then(data => this.activityResult = data);
-  }
-
-  assignActivityToEvent(): void {
-    // for (let event of this.results) {
-    //   for (let activity of this.activityResult) {
-    //     if(event.activityId = activity.activityId) {
-    //       event.activity = activity.activityDesc
-    //     }
-    //   }
-    // }
+    this.loggedIn = this.user.isLoggedIn();
+    console.log(this.currentDate);
+    // this.viewChildren.toArray().forEach((child) => child.currentDate = this.currentDate);
   }
 
   loadPrevWeek(){
-    this.viewChildren.toArray().forEach((child) => child.previous());
+    console.log("previous clicked");
+    this.currentDate.setDate(this.currentDate.getDate() - 7);
+    this.viewChildren.toArray().forEach((child) => child.setTime(this.currentDate));
+    // this.viewChildren.toArray().forEach((child) => child.previous());
+    this.viewChildren.toArray().forEach((child) => child.getAllEvents());
+    
   }
 
   loadNextWeek(){
-    this.viewChildren.toArray().forEach((child) => child.next());
-  }
+    console.log("previous clicked");
+    this.currentDate.setDate(this.currentDate.getDate() + 7);
+    this.viewChildren.toArray().forEach((child) => child.setTime(this.currentDate));
 
-  getWeek() {
-    var curr = new Date;
-    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
-    var lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 6));
-
-    console.log(firstday);
-    console.log(lastday);
-    console.log(this.results.entries);
-
-    // for (let entry of this.results){
-    //   if (entry.eventFrom >= firstday && entry.eventFrom < lastday){
-    //     this.weekResults.push(entry);
-    //     console.log("adding" + entry.eventId);
-    //   }
-    // }
+    // this.viewChildren.toArray().forEach((child) => child.next());
+    this.viewChildren.toArray().forEach((child) => child.getAllEvents());
   }
 }
--- ASP Core/ZenithSociety/ZenithClientSite/src/app/event/week/week.component.ts
+++ ASP Core/ZenithSociety/ZenithClientSite/src/app/event/week/week.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import {EventService} from '../../event.service';
 import {Event} from '../../event';
+import {EventComponent} from '../event.component'
+import {AuthenticationService} from '../../authentication.service'
+
 
 @Component({
   selector: 'app-week',
@@ -8,13 +12,19 @@ import {Event} from '../../event';
   styleUrls: ['./week.component.css']
 })
 export class WeekComponent implements OnInit {
-  results: Array<Event>;
+  results: Array<Event> = [];
+  currentWeekResults: Array<Event>
   title: "Events for the week of";
+  currentDate: Date = new Date();
 
   constructor(private eventService: EventService) {}
   
   ngOnInit() {
   	this.getAllEvents();
+  }
+
+  setTime(date){
+    this.currentDate = date;
   }
 
   getAllEvents(): void {
@@ -24,16 +34,52 @@ export class WeekComponent implements OnInit {
     //       );
 
     this.eventService.getAll()
-      .then(data => this.results = data);
-     
+      .then(data => this.parseEvents(data));  
+  }
+
+  parseEvents(eventList){
+    var typedlist: [Event] = eventList;
+
+    var curr = this.currentDate;
+    curr.setHours(0,0,0,0);
+    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay() + (curr.getDay() == 0 ?  -6 : 1)));
+    var lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 8));
+
+    console.log(firstday);
+    console.log(lastday);
+    // console.log(lastday > firstday);
+    // console.log(lastday < firstday);
+
+    for (let event of typedlist){
+
+      var fromDate = new Date(event.EventFrom);
+      var toDate = new Date(event.EventTo);
+      if (fromDate >= firstday && toDate < lastday){
+        this.results.push(event);
+        console.log(toDate);
+      }
+    }
+    
+    var sortedResults: Array<Event>;
+    sortedResults = this.results.slice(0);
+    sortedResults.sort((lhs, rhs): number => {
+      var lDate = new Date(lhs.EventFrom).getTime();
+      var rDate = new Date(rhs.EventFrom).getTime();
+      if (lDate == rDate) return 0;
+      return lDate < rDate ? -1 : 1;
+    });
+
+    this.results = sortedResults;
   }
 
   previous(){
-  	console.log("previous clicked!");
+    console.log("previous clicked");
+  	// this.currentDate.setDate(this.currentDate.getDate() - 7);
   }
 
   next(){
-  	console.log("next clicked!");
+    console.log("next clicked");
+  	// this.currentDate.setDate(this.currentDate.getDate() + 7);
   }
 
 }
